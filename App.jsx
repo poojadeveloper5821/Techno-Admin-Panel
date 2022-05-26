9cdea8470cc13a754b30f7950939970333987e4d
poojadeveloper5821 <100752964+poojadeveloper5821@users.noreply.github.com>
2022-05-26 19:06:02 +0530

    Admin panel-header page

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+//import Login from './components/Login/Login';
+import Header from './components/Header/Header';
+function App() {
+  return (
+    <div className="App">
+       {/* <Login/>  */}
+       <Header /> 
+    </div>
+  );
+}
+
+export default App;
